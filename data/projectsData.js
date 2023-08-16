766515be097e79115986e9c8d81320cb75d21573
anirbanpranto <anirbanpranto@gmail.com>
2023-08-16 18:02:09 +0800

    fix: added image

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,18 +5,6 @@ const projectsData = [
     imgSrc: '/static/images/parcel.png',
     href: 'https://parceldaily.com',
   },
-  {
-    title: 'Crab',
-    description: `Crab is Grab, but for Car Washing. Made with Express and React.`,
-    imgSrc: '/static/images/crab.png',
-    href: 'https://github.com/anirbanpranto/Crab',
-  },
-  {
-    title: 'Cyber Rent',
-    description: `A room renting desktop app made with the purpose of demonstrating OO Design Patterns in Real Life.`,
-    imgSrc: '/static/images/homepage.gif',
-    href: 'https://github.com/anirbanpranto/CyberRent',
-  },
 ]
 
 export default projectsData
